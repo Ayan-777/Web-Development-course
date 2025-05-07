7b209315eae03349934511e0164a86308311a16a
Ayan <mondalayan@gmail.com>
2025-05-07 16:12:28 +0530

    Add initial implementation of loops in JavaScript

---
+++ Tutorial 57; Loops/script.js
@@ -0,0 +1,25 @@
+console.log("Hello World");
+
+
+// let  a = 0;
+//------------- for loop-------------
+// for (let i = 0; i <= 100; i++) {
+//     console.log(a + i);
+// }
+
+
+
+//------------------ while loop--------------------
+// let i = 1;
+// while (i <= 6) {
+//     console.log(i);
+//     i++;
+// }
+
+
+//------------------ do while loop--------------------
+// let i = 10;
+// do {
+//     console.log(i)
+//     i++;
+// } while (i<6);
