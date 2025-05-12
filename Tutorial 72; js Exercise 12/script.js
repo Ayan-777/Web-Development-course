16073a62df6d1cf2c2fe34fe1b0b233b585e493e
Ayan <mondalayan@gmail.com>
2025-05-12 19:51:49 +0530

    Refactor HTML and JavaScript for improved readability and organization

--- Tutorial 72; js Exercise 12/script.js
+++ Tutorial 72; js Exercise 12/script.js
@@ -1,15 +1,18 @@
 console.log("Script.js initializing")
-// let boxes = document.getElementsByClassName("box")
+
+// Select all child elements of the container
 let boxes = document.querySelector(".container").children 
 
-
+// Function to generate a random RGB color
 function getRandomColor(){
-    let val1 = Math.ceil(0 + Math.random()* 255);
-    let val2 = Math.ceil(0 + Math.random()* 255);
-    let val3 = Math.ceil(0 + Math.random()* 255);
-    return `rgb(${val1}, ${val2}, ${val3})`
+    let val1 = Math.ceil(0 + Math.random()* 255); // Random value for Red
+    let val2 = Math.ceil(0 + Math.random()* 255); // Random value for Green
+    let val3 = Math.ceil(0 + Math.random()* 255); // Random value for Blue
+    return `rgb(${val1}, ${val2}, ${val3})` // Return the RGB color string
 }
+
+// Apply random background and text colors to each box
 Array.from(boxes).forEach(e=>{
-    e.style.backgroundColor = getRandomColor()
-    e.style.color = getRandomColor()
+    e.style.backgroundColor = getRandomColor() // Set random background color
+    e.style.color = getRandomColor() // Set random text color
 })
