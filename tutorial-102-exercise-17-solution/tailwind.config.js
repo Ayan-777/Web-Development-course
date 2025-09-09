7a2114913c0e9ad48693231e11c06956a37940e8
Ayan <mondalayan@gmail.com>
2025-09-09 16:19:42 +0530

    tutorial-102-exercise-17-solution

---
+++ tutorial-102-exercise-17-solution/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}",       // all files in root
+    "./src/**/*.{html,js}" // all files inside src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
