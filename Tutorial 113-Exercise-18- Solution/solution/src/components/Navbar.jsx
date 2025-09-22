232992ac1b1fb50dbaff4ca42c65a487f714dff2
Ayan <mondalayan@gmail.com>
2025-09-22 12:13:48 +0530

    Tutorial 113

---
+++ Tutorial 113-Exercise-18- Solution/solution/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+        <nav>
+            <ul>
+                <li>About</li>
+                <li>Home</li>
+                <li>Contact Us</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
